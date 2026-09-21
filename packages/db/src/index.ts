089247fe686417857b2395ab61d40341d7c98738
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-21 19:23:39 +0000

    feat(documents): register document schema

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -3,8 +3,9 @@ import postgres from "postgres";
 import * as authSchema from "./auth";
 import * as orgSchema from "./organization";
 import * as projectSchema from "./project";
+import * as documentsSchema from "./documents";
 
-const allSchemas = { ...authSchema, ...orgSchema, ...projectSchema };
+const allSchemas = { ...authSchema, ...orgSchema, ...projectSchema, ...documentsSchema };
 
 export type Database = PostgresJsDatabase<typeof allSchemas>;
 
@@ -18,3 +19,4 @@ export * from "./env";
 export * as schema from "./auth";
 export * as organizationSchema from "./organization";
 export * as projectSchema from "./project";
+export * as documentsSchema from "./documents";
