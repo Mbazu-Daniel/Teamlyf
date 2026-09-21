a9ea2c2c02d354e3200c504a8961ae71261fcf96
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-21 18:50:19 +0000

    feat(organization): centralize organization membership context

--- apps/api/src/modules/organization/organization.module.ts
+++ apps/api/src/modules/organization/organization.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
+import { DbModule } from "../../common/db/db.module";
+import { OrganizationAccessService } from "./organization-access.service";
 import { OrganizationController } from "./organization.controller";
 import { OrganizationService } from "./organization.service";
 
 @Module({
+  imports: [DbModule],
   controllers: [OrganizationController],
-  providers: [OrganizationService],
+  providers: [OrganizationService, OrganizationAccessService],
+  exports: [OrganizationAccessService],
 })
 export class OrganizationModule {}
