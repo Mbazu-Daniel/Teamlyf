56f22aaba84c8aa1275447127d003e2c30bc7d75
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-21 20:40:20 +0000

    feat(calls): register call module

---
+++ apps/api/src/modules/calls/call.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { EnvModule } from "../../common/config/env.module";
+import { CallController } from "./call.controller";
+import { CallService } from "./call.service";
+
+@Module({
+  imports: [EnvModule],
+  controllers: [CallController],
+  providers: [CallService],
+})
+export class CallModule {}
