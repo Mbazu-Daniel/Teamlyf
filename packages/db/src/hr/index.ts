c55dcce54bf0d31f75953da64c65ed92eacd84fb
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-21 20:39:32 +0000

    Remove attendance schema export

--- packages/db/src/hr/index.ts
+++ packages/db/src/hr/index.ts
@@ -1,4 +1,3 @@
 export { hrDepartment, hrDepartmentMember } from "./department";
 export { hrEmployeeProfile } from "./employee-profile";
 export { hrLeavePolicy, hrLeaveRequest } from "./leave";
-export { hrAttendance } from "./attendance";
