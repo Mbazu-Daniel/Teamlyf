8f745fa0e15ab54a41ff49e984de27afe30655f1
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-21 18:50:25 +0000

    feat(organization): centralize organization membership context

--- apps/api/src/modules/rbac/rbac.module.ts
+++ apps/api/src/modules/rbac/rbac.module.ts
@@ -1,12 +1,13 @@
 import { Global, Module } from "@nestjs/common";
 import { DbModule } from "../../common/db/db.module";
 import { AuthModule } from "../auth/auth.module";
+import { OrganizationModule } from "../organization/organization.module";
 import { OrgMemberGuard } from "./org-member.guard";
 import { PermissionsGuard } from "./permissions.guard";
 
 @Global()
 @Module({
-  imports: [DbModule, AuthModule],
+  imports: [DbModule, AuthModule, OrganizationModule],
   providers: [OrgMemberGuard, PermissionsGuard],
   exports: [OrgMemberGuard, PermissionsGuard],
 })
