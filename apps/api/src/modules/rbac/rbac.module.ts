659ef67b4eceafb21919a72a7d8279a0d033d2a5
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-21 21:15:22 +0000

    feat(rbac): register organization permission service

--- apps/api/src/modules/rbac/rbac.module.ts
+++ apps/api/src/modules/rbac/rbac.module.ts
@@ -2,12 +2,13 @@ import { Global, Module } from "@nestjs/common";
 import { DbModule } from "../../common/db/db.module";
 import { AuthModule } from "../auth/auth.module";
 import { OrgMemberGuard } from "./org-member.guard";
+import { OrganizationPermissionService } from "./organization-permission.service";
 import { PermissionsGuard } from "./permissions.guard";
 
 @Global()
 @Module({
   imports: [DbModule, AuthModule],
-  providers: [OrgMemberGuard, PermissionsGuard],
-  exports: [OrgMemberGuard, PermissionsGuard],
+  providers: [OrgMemberGuard, OrganizationPermissionService, PermissionsGuard],
+  exports: [OrgMemberGuard, OrganizationPermissionService, PermissionsGuard],
 })
 export class RbacModule {}
