dca5e1d04f29eca88949d921839737147b33f2ef
Daniel Mbazu <mbazudaniel97@gmail.com>
2026-09-23 19:59:58 +0100

    chore(call): fix server issues

--- apps/api/src/modules/calls/call.module.ts
+++ apps/api/src/modules/calls/call.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
 import { EnvModule } from "../../common/config/env.module";
+import { DbModule } from "../../common/db/db.module";
 import { CallController } from "./call.controller";
 import { CallService } from "./call.service";
 
-@Module({ imports: [EnvModule], controllers: [CallController], providers: [CallService] })
+@Module({ imports: [EnvModule, DbModule], controllers: [CallController], providers: [CallService] })
 export class CallModule {}
