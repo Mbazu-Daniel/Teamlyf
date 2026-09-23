01dc7f47f295fcca8001e46345de0bad50895325
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-23 21:55:56 +0000

    feat(web): export api error types

--- apps/web/src/lib/api/index.ts
+++ apps/web/src/lib/api/index.ts
@@ -2,3 +2,4 @@ export { client, request } from "./client";
 export type { Organization, RequestOptions } from "./client";
 export * from "./projects";
 export * from "./settings";
+export { ApiError, type ApiErrorCode, type ApiErrorPayload } from "./errors";
