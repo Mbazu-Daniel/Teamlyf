295f68fb1a62edd83f4c87c613380efc583bc30d
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-23 21:07:24 +0000

    refactor(web): export domain API clients

--- apps/web/src/lib/api/index.ts
+++ apps/web/src/lib/api/index.ts
@@ -1,2 +1,8 @@
 export * from "./projects";
 export * from "./settings";
+
+export * from "./documents";
+export * from "./notes";
+export * from "./hr";
+export * from "./calls";
+export * from "./chat";
