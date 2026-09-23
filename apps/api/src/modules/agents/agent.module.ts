0583da02cb73ff0b962252658fd324a2cbf6e301
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-23 07:44:22 +0000

    fix(agents): import database module

--- apps/api/src/modules/agents/agent.module.ts
+++ apps/api/src/modules/agents/agent.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { DbModule } from "../../common/db/db.module";
 import { AgentController } from "./agent.controller";
 import { AgentService } from "./agent.service";
 
 @Module({
+  imports: [DbModule],
   controllers: [AgentController],
   providers: [AgentService],
 })
