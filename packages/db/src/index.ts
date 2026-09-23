6c2d7ef2ea7a7af9ca0a4a7a65a7adfe9758a87a
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-23 07:54:05 +0000

    fix(db): resolve duplicate organization exports

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -35,7 +35,6 @@ export { generateId } from "./id";
 export * from "./env";
 export * as schema from "./auth";
 export * as organizationSchema from "./organization";
-export * from "./organization";
 export * as projectSchema from "./project";
 export * from "./project";
 export * as documentsSchema from "./documents";
