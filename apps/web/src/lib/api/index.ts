9eac0cd94e605beb0ac47ff103df272387cbdbb2
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-24 06:16:32 +0000

    refactor(web): establish typed API error boundary

---
+++ apps/web/src/lib/api/index.ts
@@ -0,0 +1,3 @@
+export { request, type RequestOptions, type Organization } from "./client";
+export { ApiError, toApiError, type ApiErrorPayload } from "./errors";
+export const client = { request };
