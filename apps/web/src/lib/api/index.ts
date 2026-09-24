1612842b285e98b7f1ea7e57950d04a501f520da
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-24 05:57:33 +0000

    refactor(web): remove unused API re-exports

--- apps/web/src/lib/api/index.ts
+++ apps/web/src/lib/api/index.ts
@@ -1,5 +1,2 @@
-export { client, request } from "./client";
-export type { Organization, RequestOptions } from "./client";
 export * from "./projects";
 export * from "./settings";
-export { ApiError, type ApiErrorCode, type ApiErrorPayload } from "./errors";
