6faed789b14b4e480deab71aa17cf1e3414535f1
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-26 14:42:00 +0100

    feat(chat): expose direct message model

--- packages/db/src/chat/index.ts
+++ packages/db/src/chat/index.ts
@@ -2,3 +2,4 @@ export { channel } from "./channel";
 export { channelMember } from "./channel-member";
 export { message } from "./message";
 export { messageReaction } from "./message-reaction";
+export { directMessage } from "./direct-message";
