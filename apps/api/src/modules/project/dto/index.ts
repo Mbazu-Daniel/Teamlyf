60be6fd868396a28691930a3a2f4b8350e958b00
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-26 10:32:20 +0100

    feat(projects): export GitHub repository dto

--- apps/api/src/modules/project/dto/index.ts
+++ apps/api/src/modules/project/dto/index.ts
@@ -1,2 +1,3 @@
 export { CreateProjectDto, UpdateProjectDto } from "./project.dto";
 export { ReorderDto } from "./reorder.dto";
+export { ConnectGithubRepositoryDto } from "./github-repository.dto";
