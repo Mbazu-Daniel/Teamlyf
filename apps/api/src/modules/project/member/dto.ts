d50ae6fbc81f24658f2139c1d6f908019e970a20
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-26 13:51:39 +0100

    fix(api): validate project member UUIDs

--- apps/api/src/modules/project/member/dto.ts
+++ apps/api/src/modules/project/member/dto.ts
@@ -1,8 +1,8 @@
-import { IsArray, IsString, ArrayMinSize } from "class-validator";
+import { ArrayMinSize, IsArray, IsUUID } from "class-validator";
 
 export class AddProjectMembersDto {
   @IsArray()
   @ArrayMinSize(1)
-  @IsString({ each: true })
+  @IsUUID("all", { each: true })
   memberIds!: string[];
 }
