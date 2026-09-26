3f6aeb17a449e36ed495dbc3083b0fa83b1d98c9
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-26 11:02:56 +0100

    feat(api): wire GitHub App into projects

--- apps/api/src/modules/project/project.module.ts
+++ apps/api/src/modules/project/project.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { ProjectSharedModule } from "./project-shared.module";
 import { ProjectController } from "./project.controller";
 import { ProjectService } from "./project.service";
+import { GithubAppModule } from "./github-app.module";
 
 @Module({
-  imports: [ProjectSharedModule],
+  imports: [ProjectSharedModule, GithubAppModule],
   controllers: [ProjectController],
   providers: [ProjectService],
 })
