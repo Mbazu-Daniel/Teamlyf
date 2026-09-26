2085207a93c9bbd8f98141cde22c1b1662d76e7b
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-26 14:43:07 +0100

    feat(chat): register modular direct messaging

--- apps/api/src/modules/chat/chat.module.ts
+++ apps/api/src/modules/chat/chat.module.ts
@@ -3,16 +3,17 @@ import { DbModule } from "../../common/db/db.module";
 import { ChatController } from "./chat.controller";
 import { ChatService } from "./chat.service";
 import { ChatUuidMiddleware } from "./chat-uuid.middleware";
+import { DirectMessageController } from "./direct/direct-message.controller";
+import { DirectMessageService } from "./direct/direct-message.service";
 
 @Module({
   imports: [DbModule],
-  controllers: [ChatController],
-  providers: [ChatService],
+  controllers: [ChatController, DirectMessageController],
+  providers: [ChatService, DirectMessageService],
+  exports: [ChatService, DirectMessageService],
 })
 export class ChatModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(ChatUuidMiddleware)
-      .forRoutes({ path: "organization/:orgId/channels", method: RequestMethod.ALL }, ChatController);
+    consumer.apply(ChatUuidMiddleware).forRoutes({ path: "organization/:orgId/channels", method: RequestMethod.ALL }, ChatController);
   }
 }
